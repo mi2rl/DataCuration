9829408db229799d658258b8f2462b9be85695c4
JUNG, JIHOON <jhooons3327@gmail.com>
2024-10-18 04:50:29 +0000

    Upload new file

--- Brainmeta_Seg/brainmeta_prediction.sh
+++ Brainmeta_Seg/brainmeta_prediction.sh
@@ -5,14 +5,14 @@ CURRENT_DIR=$(dirname $(realpath $0))
 ###########################################################################################################
 # manual typing
 INPUT_DIR="$CURRENT_DIR/_inputs"
-OUTPUT_DIR="$CURRENT_DIR/_results" # 최종 결과 저장 경로. 해당 폴더가 없으면 자동으로 생성.
+OUTPUT_DIR="$CURRENT_DIR/_results" 
 GPUS="0"
 ###########################################################################################################
 
-# 혹시 실행이 안될 시 아래 코드의 주석을 풀어주세요.
-# export PATH=$PATH:~/.local/bin
 pip install -e $CURRENT_DIR/
 
+mkdir -p "$CURRENT_DIR/nnUNet_raw"
+mkdir -p "$CURRENT_DIR/nnUNet_preprocessed"
 export nnUNet_raw="$CURRENT_DIR/nnUNet_raw"
 export nnUNet_preprocessed="$CURRENT_DIR/nnUNet_preprocessed"
 export nnUNet_results="$CURRENT_DIR/nnUNet_results"
@@ -20,13 +20,11 @@ export MKL_SERVICE_FORCE_INTEL=1
 
 STAGE1_RESULTS="$OUTPUT_DIR/previous_stage_results"
 
-# results 폴더 안에 .nii.gz파일이 있으면 warning을 띄우고, 동작을 멈춘다.
 if ls $OUTPUT_DIR/*.nii.gz 1> /dev/null 2>&1; then
     echo "Error: .nii.gz files already exist in the results folder. empty the results folder and try again."
     exit 1
 fi
 
-# input file 이름이 _0000.nii.gz로 끝나지 않으면 _0000.nii.gz로 바꿔준다.
 for file in $INPUT_DIR/*.nii.gz; do
     if [[ ! $file =~ "_0000.nii.gz" ]]; then
         mv "$file" "${file%.nii.gz}_0000.nii.gz"
